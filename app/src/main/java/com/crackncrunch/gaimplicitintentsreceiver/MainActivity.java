d03e3e921517291ea9a899a8ec37439366f9639c
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-05-13 21:47:02 +0400

    Complete projects

--- app/src/main/java/com/crackncrunch/gaimplicitintentsreceiver/MainActivity.java
+++ app/src/main/java/com/crackncrunch/gaimplicitintentsreceiver/MainActivity.java
@@ -1,7 +1,10 @@
 package com.crackncrunch.gaimplicitintentsreceiver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String uriString = "URI: " + uri.toString();
+            TextView textView = (TextView) findViewById(R.id.text_uri_message);
+            textView.setText(uriString);
+        }
     }
 }
